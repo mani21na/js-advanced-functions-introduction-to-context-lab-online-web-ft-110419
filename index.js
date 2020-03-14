1624947c4ea660b071d818c87debff6963b79fe6
Hanna Lee <mani21na@gmail.com>
2020-03-14 04:47:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+let createEmployeeRecord = fuction(row) {
+  return {
+    firstName: row[0],
+    familyName: row[1],
+    title: row[2],
+    payPerHour: row[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+} 
